b3b48c3fc8e819e95f94b8a4313bd4af63421eac
Galfest <abramyants4@gmail.com>
2023-01-16 09:45:57 +0300

    Task 13

---
+++ src/main/java/Task13/Dankey.java
@@ -0,0 +1,7 @@
+package Task13;
+
+public class Dankey extends PackAnimals{
+    private final boolean run = true;
+    private final boolean voice = true;
+    private final boolean jump = true;
+}
---
+++ src/main/java/Task13/Hors.java
@@ -0,0 +1,7 @@
+package Task13;
+
+public class Hors extends PackAnimals{
+    private final boolean run = true;
+    private final boolean voice = true;
+    private final boolean jump = true;
+}
